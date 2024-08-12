916f6180249693947f41532ede8f0633b2323df0
Hadley Wickham <h.wickham@gmail.com>
2024-08-12 14:12:07 -0700

    Extract fuction; add defaults

--- app.R
+++ app.R
@@ -6,13 +6,13 @@ ui <- fluidPage(
 
   sidebarLayout(
     sidebarPanel(
-      textInput("noun1", "Enter a noun:"),
-      textInput("adj1", "Enter an adjective:"),
-      textInput("verb1", "Enter a verb (past tense):"),
-      textInput("adverb1", "Enter an adverb:"),
-      textInput("noun2", "Enter another noun:"),
-      textInput("adj2", "Enter another adjective:"),
-      textInput("verb2", "Enter another verb:"),
+      textInput("noun1", "Enter a noun:", value = "cat"),
+      textInput("adj1", "Enter an adjective:", value = "fluffy"),
+      textInput("verb1", "Enter a verb (past tense):", value = "jumped"),
+      textInput("adverb1", "Enter an adverb:", value = "quickly"),
+      textInput("noun2", "Enter another noun:", value = "tree"),
+      textInput("adj2", "Enter another adjective:", value = "tall"),
+      textInput("verb2", "Enter another verb:", value = "climb"),
       actionButton("generate", "Generate Story!")
     ),
 
@@ -26,19 +26,32 @@ ui <- fluidPage(
 # Define the server logic
 server <- function(input, output) {
 
-  story <- eventReactive(input$generate, {
-    paste0("Once upon a time, there was a ", input$adj1, " ", input$noun1, " who ",
-           input$verb1, " ", input$adverb1, " through the enchanted forest. ",
-           "Suddenly, a magical ", input$noun2, " appeared! ",
-           "The ", input$noun1, " was so surprised that they immediately ",
-           input$verb2, ". ",
-           "In the end, everyone lived happily ever after in a ",
-           input$adj2, " castle.")
-  })
 
+
+  story <- eventReactive(input$generate, {
+    generateStory(
+      input$noun1,
+      input$adj1,
+      input$verb1,
+      input$adverb1,
+      input$noun2,
+      input$adj2,
+      input$verb2
+    )
+  })
   output$story <- renderText({
     story()
   })
+}
+
+generateStory <- function(noun1, adj1, verb1, adverb1, noun2, adj2, verb2) {
+  paste0("Once upon a time, there was a ", adj1, " ", noun1, " who ",
+         verb1, " ", adverb1, " through the enchanted forest. ",
+         "Suddenly, a magical ", noun2, " appeared! ",
+         "The ", noun1, " was so surprised that they immediately ",
+         verb2, ". ",
+         "In the end, everyone lived happily ever after in a ",
+         adj2, " castle.")
 }
 
 # Run the app
